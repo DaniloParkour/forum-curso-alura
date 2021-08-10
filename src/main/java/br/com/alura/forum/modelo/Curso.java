7a4ea5dc50950065ba152f04d9b195b08948c698
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-08-10 17:28:05 -0300

    Configuracao da JPA e definição das entidades

--- src/main/java/br/com/alura/forum/modelo/Curso.java
+++ src/main/java/br/com/alura/forum/modelo/Curso.java
@@ -1,7 +1,16 @@
 package br.com.alura.forum.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Curso {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//GenerationType.IDENTITY => É um AUTO INCREMENT. Se o banco usar, pode ser também SEQUENCE
 	private Long id;
 	private String nome;
 	private String categoria;

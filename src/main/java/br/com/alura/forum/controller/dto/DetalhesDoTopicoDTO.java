995f7b090b65d3621c3e618fc19ef6f103d9c69c
Danilo Carvalho <danilomlcarvalho@gmail.com>
2021-08-11 15:57:09 -0300

    Faltou os GETs para serialização funcionar

--- src/main/java/br/com/alura/forum/controller/dto/DetalhesDoTopicoDTO.java
+++ src/main/java/br/com/alura/forum/controller/dto/DetalhesDoTopicoDTO.java
@@ -19,14 +19,42 @@ public class DetalhesDoTopicoDTO {
 	private List<RespostaDTO> respostas;
     
     public DetalhesDoTopicoDTO(Topico topico) {
-        this.id = topico.getId();
-            this.titulo = topico.getTitulo();
-            this.mensagem = topico.getMensagem();
-            this.dataCriacao = topico.getDataCriacao();
-            this.nomeAutor = topico.getAutor().getNome();
-            this.status = topico.getStatus();
-            this.respostas = new ArrayList<>();
-            this.respostas.addAll(topico.getRespostas().stream().map(RespostaDTO::new).collect(Collectors.toList()));
-        }
+    	this.id = topico.getId();
+        this.titulo = topico.getTitulo();
+        this.mensagem = topico.getMensagem();
+        this.dataCriacao = topico.getDataCriacao();
+        this.nomeAutor = topico.getAutor().getNome();
+        this.status = topico.getStatus();
+        this.respostas = new ArrayList<>();
+        this.respostas.addAll(topico.getRespostas().stream().map(RespostaDTO::new).collect(Collectors.toList()));
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+
+	public StatusTopico getStatus() {
+		return status;
+	}
+
+	public List<RespostaDTO> getRespostas() {
+		return respostas;
+	}
 
 }
